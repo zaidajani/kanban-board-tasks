1494305b0cc58019445c5d46e43eb96d67f67908
zaidajani <zaidajani2007@gmail.com>
2026-06-06 16:14:34 +0530

    Added backend env variable

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -1,6 +1,6 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 
-const API_BASE = process.env.REACT_APP_API_BASE || "http://localhost:5000/api";
+const API_BASE = process.env.REACT_APP_API_BASE;
 const AppContext = createContext(null);
 
 export function AppProvider({ children }) {
